481f21c12d1e4d209a997b38c09610c7a6686cfb
Fernando <manuelhola9@gmail.com>
2025-05-10 14:33:51 -0700

    feat: add isActive field to Category model; enhance CategoryService with existence check during creation; update CreateCategoryDto with validation rules

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,26 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { DbService } from 'src/db/db.service';
 
 @Injectable()
 export class CategoryService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+
+  private readonly logger = new Logger(CategoryService.name);
+
+  constructor(
+    private readonly dbService: DbService
+  ){}
+
+  async create(createCategoryDto: CreateCategoryDto) {
+  
+    try {
+    
+      // Check if the category already exists
+      const existingCategory = await this.dbService.category.findUnique({
+        where: {
+          name: createCategoryDto.name
+        }
+      });
+
+      if (existingCategory) {
+        this.logger.warn(`Category with name ${createCategoryDto.name} already exists`);
+        return;
+      }
+      
+      // Create the new category
+      await this.dbService.category.create({
+        data: {
+          name: createCategoryDto.name,
+          description: createCategoryDto.description ? createCategoryDto.description : 'No description',
+        }
+      });
+
+      return {
+        message: 'Category created successfully',
+      };
+
+    } catch (error) {
+      
+    }
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all category`;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} category`;
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
+  async update(id: number, updateCategoryDto: UpdateCategoryDto) {
     return `This action updates a #${id} category`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} category`;
   }
 }
--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,1 +1,17 @@
-export class CreateCategoryDto {}
+import { IsNotEmpty, IsOptional, IsString, Max, Min } from "class-validator";
+
+export class CreateCategoryDto {
+
+    @IsString()
+    @IsNotEmpty()
+    @Min(3)
+    @Max(50)
+    name: string; // Nombre de la categoría
+    
+    @IsString()
+    @Min(3)
+    @Max(50)
+    @IsNotEmpty()
+    @IsOptional()
+    description?: string; // Descripción de la categoría
+}
