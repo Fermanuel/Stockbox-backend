89e80c35346debea30550681a14b6d98bc7a1d79
Fernando <manuelhola9@gmail.com>
2025-05-10 11:24:39 -0700

    feat: implement role management with CreateRoleDto and UpdateRoleDto

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { DbModule } from './db/db.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { RoleModule } from './role/role.module';
 
 @Module({
   imports: [
     DbModule, 
     AuthModule, 
     ConfigModule.forRoot({}), UserModule,
-    UserModule
+    UserModule,
+    RoleModule
   ],
 })
 export class AppModule {}
