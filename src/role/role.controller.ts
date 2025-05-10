92959ab510d69f8616517cecfc3bea78b8f4c140
Fernando <manuelhola9@gmail.com>
2025-05-10 11:49:57 -0700

    feat: add authentication decorators to role controller methods

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -2,33 +2,44 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from 
 import { RoleService } from './role.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Auth } from 'src/auth/decorators/auth.decorator';
 
 @ApiTags('Role')
 @Controller('role')
 export class RoleController {
   constructor(private readonly roleService: RoleService) {}
 
+  @ApiBearerAuth()
+  @Auth('Administrador')
   @Post()
   create(@Body() createRoleDto: CreateRoleDto) {
     return this.roleService.create(createRoleDto);
   }
 
+  @ApiBearerAuth()
+  @Auth('Administrador')
   @Get()
   findAll() {
     return this.roleService.findAll();
   }
 
+  @ApiBearerAuth()
+  @Auth('Administrador')
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.roleService.findOne(id);
   }
 
+  @ApiBearerAuth()
+  @Auth('Administrador')
   @Patch(':id')
   update(@Param('id', ParseIntPipe) id: number, @Body() updateRoleDto: UpdateRoleDto) {
     return this.roleService.update(id, updateRoleDto);
   }
 
+  @ApiBearerAuth()
+  @Auth('Administrador')
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.roleService.remove(+id);
