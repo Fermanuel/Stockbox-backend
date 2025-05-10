b8256c6909674b107eb429dbef542c8a6302ecfe
Fernando <manuelhola9@gmail.com>
2025-05-10 13:58:02 -0700

    feat: implement Category module with controller, service, and DTOs for category management

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { RoleModule } from './role/role.module';
 import { ProductModule } from './product/product.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
@@ -13,7 +14,8 @@ import { ProductModule } from './product/product.module';
     ConfigModule.forRoot({}), UserModule,
     UserModule,
     RoleModule,
-    ProductModule
+    ProductModule,
+    CategoryModule
   ],
 })
 export class AppModule {}
